2417d7ce7da80e657337a679fb216209dc7d587f
Bence Bardosi <bardosi.bence@gmail.com>
2016-12-22 15:09:04 +0100

    comments

---
+++ comments/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    println!("5+1= {}",add_one(5));
+}
+
+/// Adds one to the number given.
+///
+/// # Examples
+///
+/// ```
+/// let five = 5;
+///
+/// assert_eq!(6, add_one(5));
+/// # fn add_one(x: i32) -> i32 {
+/// #     x + 1
+/// # }
+/// ```
+fn add_one(x: i32) -> i32 {
+    x + 1
+}

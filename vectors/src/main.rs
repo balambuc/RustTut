a252a663cdbf29ffe40d3e0a46e0d969a3024868
Bence Bardosi <bardosi.bence@gmail.com>
2017-01-09 18:18:41 +0100

    Vectors

---
+++ vectors/src/main.rs
@@ -0,0 +1,26 @@
+fn main() {
+    let mut v = vec![1,2,3,4,5];
+    let v_rep = vec![0;5];
+    let mut i: usize = 0; //vektor indexelés csak usize lehet
+    while i<5
+    {
+        println!("{:?}, {:?}",v[i], v_rep[i] );
+        i+=1;
+    }
+
+    for i in &v {
+    println!("A reference to {}", i);
+    }
+
+    for i in &mut v {
+        println!("A mutable reference to {}", i);
+    }
+
+    for i in v {
+        println!("Take ownership of the vector and its element {}", i);
+    }
+
+    /*for i in v {
+        println!("Take ownership of the vector and its element {}", i);
+    }*/ //Nem lehet ownership csere után használni
+}

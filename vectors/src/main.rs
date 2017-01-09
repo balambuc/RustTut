9009963f3a4848fd2330061cbef295817d4456fd
Bence Bardosi <bardosi.bence@gmail.com>
2017-01-09 18:27:33 +0100

    Vec_englishified

--- vectors/src/main.rs
+++ vectors/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     let mut v = vec![1,2,3,4,5];
     let v_rep = vec![0;5];
-    let mut i: usize = 0; //vektor indexelés csak usize lehet
+    let mut i: usize = 0; //vektor index can only be usize
     while i<5
     {
         println!("{:?}, {:?}",v[i], v_rep[i] );
@@ -22,5 +22,5 @@ fn main() {
 
     /*for i in v {
         println!("Take ownership of the vector and its element {}", i);
-    }*/ //Nem lehet ownership csere után használni
+    }*/ //Can't use after taking ownership
 }
